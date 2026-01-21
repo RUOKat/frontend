dbde07c22ba4cd9bc92a7d7cdb73384ac5a587b2
monkeymagician <221csw2@gmail.com>
2026-01-21 09:32:02 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', ## 최승우가 테스트용으로 추가
+  output: 'standalone', 
   typescript: {
     ignoreBuildErrors: true,
   },
